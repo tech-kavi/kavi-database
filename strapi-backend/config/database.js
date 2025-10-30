3107da162a4b4495e2d5af22a8a7b875dcd7f9de
nishant <tech@kaviresearch.in>
2025-10-30 19:19:01 +0530

    change in database config

--- strapi-backend/config/database.js
+++ strapi-backend/config/database.js
@@ -30,7 +30,7 @@ module.exports = ({ env }) => {
     database: env('DATABASE_NAME', 'strapi'),
     user: env('DATABASE_USERNAME', 'strapi'),
     password: env('DATABASE_PASSWORD', 'strapi'),
-    ssl: {
+    ssl: env.bool('DATABASE_SSL', false) && {
       rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
     },
     schema: env('DATABASE_SCHEMA', 'public'),
