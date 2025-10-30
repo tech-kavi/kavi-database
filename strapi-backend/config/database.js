72c6ae8842900dab7bf5ae22fd2bfdca5009803d
nishant <tech@kaviresearch.in>
2025-10-30 19:00:31 +0530

    changed database config for connection termination error

--- strapi-backend/config/database.js
+++ strapi-backend/config/database.js
@@ -23,36 +23,31 @@ module.exports = ({ env }) => {
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 6543),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      acquireConnectionTimeout:5000,
-      pool: { 
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 50),
-        acquireTimeoutMillis: 8000,
-        idleTimeoutMillis: 8000, // 👈 close idle connections early (before Supabase does)
-        createTimeoutMillis: 8000,
-        createRetryIntervalMillis: 100,
-        reapIntervalMillis:1000,
-        propagateCreateError: false, // 👈 prevents crash on connection drops
-      },
-      debug:false,
+  connection: {
+    connectionString: env('DATABASE_URL'),
+    host: env('DATABASE_HOST', 'localhost'),
+    port: env.int('DATABASE_PORT', 5432),
+    database: env('DATABASE_NAME', 'strapi'),
+    user: env('DATABASE_USERNAME', 'strapi'),
+    password: env('DATABASE_PASSWORD', 'strapi'),
+    ssl: {
+      rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
     },
+    schema: env('DATABASE_SCHEMA', 'public'),
+  },
+  acquireConnectionTimeout: 60000,
+  pool: { 
+    min: env.int('DATABASE_POOL_MIN', 2),
+    max: env.int('DATABASE_POOL_MAX', 20),
+    acquireTimeoutMillis: 60000,
+    idleTimeoutMillis: 30000,
+    createTimeoutMillis: 30000,
+    createRetryIntervalMillis: 100,
+    reapIntervalMillis: 1000,
+    propagateCreateError: false,
+  },
+  debug: false,
+},
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
