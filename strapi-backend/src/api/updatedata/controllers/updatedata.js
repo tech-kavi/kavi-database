5b0b37cb344b9858ea13109218757fe61e950d2a
nishant <tech@kaviresearch.in>
2026-06-03 17:04:30 +0530

    updated updatedata api

--- strapi-backend/src/api/updatedata/controllers/updatedata.js
+++ strapi-backend/src/api/updatedata/controllers/updatedata.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   find: async (ctx, next) => {
-    try {
+  try {
     const { from, to, page = 1, pageSize = 10 } = ctx.query;
 
     const filters = {
@@ -17,46 +17,47 @@ module.exports = {
       },
     };
 
-    // ✅ Add date range filter if provided
     if (from || to) {
       filters.last_update.time = {
-        ...(from ? { $gte: new Date(from) } : {}),
-        ...(to ? { $lte: new Date(to) } : {}),
-        $notNull: true,
+        ...(from ? { $gte: new Date(from + "T00:00:00.000Z") } : {}),
+        ...(to ? { $lte: new Date(to + "T23:59:59.999Z") } : {}),
       };
     }
 
-    const experts = await strapi.db.query('api::expert.expert').findMany({
-      where: filters,
-      orderBy: {
-        last_update: {
-          time: 'desc',
-        },
-      },
-      offset: (page - 1) * pageSize,
-      limit: pageSize,
-      populate: {
-        last_update: true,
-        expert_experiences:true,
-      },
-    });
+    const pageNum = Number(page);
+    const limit = Number(pageSize);
+    const start = (page-1)*limit;
 
-    // optional: total count for pagination UI
-    const total = await strapi.db.query('api::expert.expert').count({
-      where: filters,
-    });
+  const data = await strapi.documents("api::expert.expert").findMany({
+  filters,
+  sort:'last_update.time:desc',
+  limit,
+  start,
+  populate: {
+    last_update: true,
+    expert_experiences: true,
+  },
+});
 
-    return {
-      data: experts,
-      pagination: {
-        page: Number(page),
-        pageSize: Number(pageSize),
-        total,
-        pageCount: Math.ceil(total / pageSize),
-      },
-    };
+
+
+const total = await strapi.documents("api::expert.expert").count({
+  filters,
+});
+
+return {
+  data,
+  pagination: {
+    start,
+    limit,
+    total,
+    page: Math.floor(start / limit) + 1,
+    pageCount: Math.ceil(total / limit),
+  },
+};
+
   } catch (err) {
     return ctx.badRequest(err.message);
   }
-  }
+}
 };
